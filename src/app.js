9e5b9ef405766a0fdab9ae17895d72ed044887d2
Vipul Garg <vgarg10@sapient.com>
2018-04-25 15:32:23 +0530

    CommentBox - ReactApp

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import jQuery from 'jQuery';
+
+import {hashHistory, Router, Route, Redirect} from 'react-router';
+
+import Layout from './layout/layout';
+
+import BlogPage from './pages/blog'
+import PicturePage from './pages/picture'
+import VideoPage from './pages/video'
+
+const app = (
+    <Router history={hashHistory}>
+        <Redirect from='/' to='/blog' />
+        <Route path = '/' component = {Layout}>
+            <Route path = 'blog' component = {BlogPage} />
+            <Route path = 'picture' component = {PicturePage} />
+            <Route path = 'video' component = {VideoPage} />
+        </Route>
+    </Router>
+)
+
+jQuery(function() {
+  ReactDOM.render(
+    app,
+    document.getElementById('comment-box')
+  );
+})
